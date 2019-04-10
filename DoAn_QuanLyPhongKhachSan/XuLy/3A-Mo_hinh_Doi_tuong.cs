3313a992fd2b401fc95d38baa61f22340d80dc69
joytrinh <mongtuyen149@gmail.com>
2019-04-10 21:53:29 +0700

    Create 3A-MoHinhDoiTuong with some classes

--- DoAn_QuanLyPhongKhachSan/XuLy/3A-Mo_hinh_Doi_tuong.cs
+++ DoAn_QuanLyPhongKhachSan/XuLy/3A-Mo_hinh_Doi_tuong.cs
@@ -1,9 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
-public class _3A_Mo_hinh_Doi_tuong
+public class XL_KhuVuc
 {
-
+    string MaSo, Ten;
+    int SoTang;
+}
+public class XL_TiepTan
+{
+    string MaSo, HoTen, TenDangNhap, MatKhau;
+    XL_KhuVuc KhuVuc = new XL_KhuVuc();
+}
+public class XL_QuanLyKhuVuc
+{
+    string MaSo, HoTen, TenDangNhap, MatKhau;
+    List<XL_KhuVuc> DanhSachKhuVuc = new List<XL_KhuVuc>();
+}
+public class XL_Phong
+{
+    string MaSo, Ten, TrangThai;
+    XL_LoaiPhong LoaiPhong = new XL_LoaiPhong();
+    XL_KhuVuc KhuVuc = new XL_KhuVuc();
+}
+public class XL_LoaiPhong
+{
+    string MaSo, Ten, TienNghi;
+    int DonGia, SoKhachToiDa;
+}
+public class XL_PhieuThuePhong
+{
+    string MaSo, TenPhong;
+    DateTime NgayBatDau, NgayDuKienTra, NgayTraPhong;
+    List<XL_KhachHang> DanhSachKhachHang = new List<XL_KhachHang>();
+}
+public class XL_KhachHang
+{
+    string HoTen, CMND;
 }
