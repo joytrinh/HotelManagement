b0403607f3b5cb7a3c75fd02956942917547475d
joytrinh <mongtuyen149@gmail.com>
2019-04-20 17:55:46 +0700

    Hoan thanh chuc nang Tra Cuu va Xem thong tin cua Tiep Tan

--- DoAn_QuanLyPhongKhachSan/XuLy/3B-Xu_ly_3L.cs
+++ DoAn_QuanLyPhongKhachSan/XuLy/3B-Xu_ly_3L.cs
@@ -9,7 +9,7 @@ using Newtonsoft.Json;
 
 public class XL_TheHien
 {
-    public static string Dia_chi_Media = $"../../../Media";
+    public static string Dia_chi_Media = @"D:\STUDY\IT\Semester3\CNPM\Program\DoAn_QuanLyPhongKhachSan\Media";
     public static CultureInfo Dinh_dang_VN = CultureInfo.GetCultureInfo("vi-VN");
 
     public static string TaoChuoiHTMLDanhSachPhong(List<XL_Phong> DanhSachPhong)
@@ -23,13 +23,15 @@ public class XL_TheHien
             else
                 TrangThai = "Còn trống";
             var ChuoiHinh = $"<img  src='{Dia_chi_Media}/{Phong.MaSo}.png' " +
-                                     $" style='width:30px;height:30px' />";
+                                     $" style='width:50px;height:50px' />";
             var ChuoiThongTin = $"<div class='btn' style='text-align:left'> " +
                             $"<br />Tên phòng: {Phong.Ten}" +
                             $"<br />Trạng thái:{TrangThai}" +
                             $"<br />Loại phòng: {Phong.LoaiPhong.Ten }" +
-                            $"<br />{Phong.KhuVuc.Ten}" +
-                            $"</div>";
+                            $"<br />Đơn giá: {Phong.LoaiPhong.DonGia.ToString("n0", Dinh_dang_VN) }/đêm" +
+                            $"<br />Số khách tối đa: {Phong.LoaiPhong.SoKhachToiDa } người" +
+                            $"<br />Tiện nghi: {Phong.LoaiPhong.TienNghi }" +
+                            $"<br />{Phong.KhuVuc.Ten}" + $"</div>";
             var ChuoiHTML = $"<div class='col-md-3' style='margin-bottom:10px' >" +
                                $"{ChuoiHinh}" + $"{ChuoiThongTin}" +
                              "</div>";
@@ -45,6 +47,39 @@ public class XL_NghiepVu
     {
         var TiepTan = DanhSach.FirstOrDefault(x => x.TenDangNhap == TenDangNhap && x.MatKhau == MatKhau);
         return TiepTan;
+    }
+    public static List<XL_Phong> TraCuuThongTinPhong(string ChuoiTraCuu, List<XL_Phong> DanhSach)
+    {
+        var DanhSachKQ = new List<XL_Phong>();
+        int number = 2;
+        if (ChuoiTraCuu.ToUpper().Contains("Còn trống"))
+            number = 0;
+        else if (ChuoiTraCuu.ToUpper().Contains("Đã cho thuê"))
+            number = 1;
+        if (number != 2)
+        {
+            DanhSachKQ = DanhSach.FindAll(x => x.TrangThai == number);
+        }
+        else if (Int32.TryParse(ChuoiTraCuu, out number))
+        {
+            if (number < 5)
+            {
+                DanhSachKQ = DanhSach.FindAll(x => x.LoaiPhong.SoKhachToiDa == number);
+            }
+            else
+            {
+                DanhSachKQ = DanhSach.FindAll(x => x.LoaiPhong.DonGia == number);
+            }            
+        }
+        else
+        {
+            DanhSachKQ = DanhSach.FindAll(x => x.MaSo.ToUpper().Contains(ChuoiTraCuu.ToUpper()) ||
+                        x.Ten.ToUpper().Contains(ChuoiTraCuu.ToUpper()) ||
+                        x.KhuVuc.MaSo.ToUpper().Contains(ChuoiTraCuu.ToUpper()) || x.KhuVuc.Ten.ToUpper().Contains(ChuoiTraCuu.ToUpper()) ||
+                         x.LoaiPhong.Ten.ToUpper().Contains(ChuoiTraCuu.ToUpper()) || x.LoaiPhong.MaSo.ToUpper().Contains(ChuoiTraCuu.ToUpper()) ||
+                         x.LoaiPhong.TienNghi.ToUpper().Contains(ChuoiTraCuu.ToUpper()));
+        }
+        return DanhSachKQ;
     }
 }
 public class XL_LuuTru
@@ -67,7 +102,7 @@ public class XL_LuuTru
         });
         return DanhSach;
     }
-    public static List<XL_Phong> DocDanhSachPhong()
+    public static List<XL_Phong> DocDanhSachPhongTiepTanQuanLy(XL_TiepTan TiepTan)
     {
         var DanhSach = new List<XL_Phong>();
         var DanhSachTapTin = ThuMucPhong.GetFiles("*.json").ToList();
@@ -76,14 +111,9 @@ public class XL_LuuTru
             var DuongDan = TapTin.FullName;
             var chuoiJSon = File.ReadAllText(DuongDan);
             var DoiTuong = JsonConvert.DeserializeObject<XL_Phong>(chuoiJSon);
-            DanhSach.Add(DoiTuong);
+            if (DoiTuong.KhuVuc.MaSo == TiepTan.KhuVuc.MaSo)
+                DanhSach.Add(DoiTuong);
         });
-        return DanhSach;
-    }
-    public static List<XL_Phong> DanhSachPhongTheoKhuVuc(XL_TiepTan TiepTan)
-    {
-        var DanhSachPhong = DocDanhSachPhong();
-        var DanhSach = DanhSachPhong.FindAll(x => x.KhuVuc.MaSo == TiepTan.KhuVuc.MaSo);
         return DanhSach;
     }
 }
